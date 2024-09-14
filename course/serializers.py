d4bb908e6488a1668510129e6cd1d9829ea8a214
alirezayn <alireza.yn@outlook.com>
2024-09-14 12:56:43 +0330

    serializers.py update

--- course/serializers.py
+++ course/serializers.py
@@ -43,7 +43,7 @@ class CourseSerializer(serializers.ModelSerializer):
 class CourseNameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['id', 'name', 'is_pro']
+        fields = ['id', 'name', 'is_pro','image']
 
 
 
