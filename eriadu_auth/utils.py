b5754c22e08d9eb494099e374699da7af3d0cb40
alirezayn <alireza.yn@outlook.com>
2024-10-07 11:55:21 +0330

    views.py

--- eriadu_auth/utils.py
+++ eriadu_auth/utils.py
@@ -1,35 +1,32 @@
-import http.client
-import json
+import requests
 import logging
 
 
 def send_verification_code(mobile, code):
-    conn = http.client.HTTPSConnection("api.sms.ir")
-    
-    payload = json.dumps({
-        "mobile": mobile,
-        "templateId": 300543,  # Replace this with your actual template ID
-        "parameters": [
-            {
-                "name": "code",
-                "value": code
-            }
-        ]
-    })
-    
+    url = 'https://api.sms.ir/v1/send/verify'
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'text/plain',
-        'x-api-key': 'rX4ZsdADrnaDjrdpr8eG4rBW25lElddcvmMGjbIZvbOj2YJQqeSeODq78twjpT2e'  # Replace with your actual API key
+        'x-api-key': 'rX4ZsdADrnaDjrdpr8eG4rBW25lElddcvmMGjbIZvbOj2YJQqeSeODq78twjpT2e',
+    }
+    post_data = {
+        'mobile': mobile,
+        'templateId': '300543',
+        'parameters': [
+            {
+                'name': 'code',
+                'value': code
+            }
+        ]
     }
     
     try:
-        conn.request("POST", "/v1/send/verify", payload, headers)
-        res = conn.getresponse()
-        data = res.read()
-        response_body = data.decode("utf-8")
+        response = requests.post(url, json=post_data, headers=headers)
+        print(response.status_code)
+        response.raise_for_status()
+        response_body = response.text
         logging.info(f'SMS sent successfully: {response_body}')
         return response_body
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         logging.error(f'Failed to send SMS: {e}')
         return False
