728fa9e25ee054c2f0aa90c9661127b2efcd0102
alirezayn <alireza.yn@outlook.com>
2024-10-13 21:11:15 +0330

    views.py

--- eriadu_auth/views.py
+++ eriadu_auth/views.py
@@ -37,14 +37,6 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 class LoginPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
-
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 96c457ef78fec5f13d4dde6cee8534de9330bd51
 class CreateUserView(generics.CreateAPIView):
     model = user
     serializer_class = CustomUserRegisterSerializer
